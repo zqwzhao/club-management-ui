fa53592c5a35cd5dfd50f3410b768181cff23deb
赵庆武 <zhaoqw@brains-info.com>
2022-03-15 18:53:00 +0800

    添加轮播图前端页面

--- src/views/main-navbar.vue
+++ src/views/main-navbar.vue
@@ -2,7 +2,7 @@
   <nav class="site-navbar" :class="'site-navbar--' + navbarLayoutType">
     <div class="site-navbar__header">
       <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })">
-        <a class="site-navbar__brand-lg" href="javascript:;">校园社团管理系统</a>
+        <a class="site-navbar__brand-lg" href="javascript:;">校园社团管理系统后台</a>
         <a class="site-navbar__brand-mini" href="javascript:;">校园</a>
       </h1>
     </div>
---
+++ src/views/modules/slide/slide-add-or-update.vue
@@ -0,0 +1,102 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+    <el-form-item label="" prop="imageName">
+      <el-input v-model="dataForm.imageName" placeholder=""></el-input>
+    </el-form-item>
+    <el-form-item label="" prop="imageUrl">
+      <el-input v-model="dataForm.imageUrl" placeholder=""></el-input>
+    </el-form-item>
+    <el-form-item label="" prop="imageIndex">
+      <el-input v-model="dataForm.imageIndex" placeholder=""></el-input>
+    </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        visible: false,
+        dataForm: {
+          id: 0,
+          imageName: '',
+          imageUrl: '',
+          imageIndex: ''
+        },
+        dataRule: {
+          imageName: [
+            { required: true, message: '不能为空', trigger: 'blur' }
+          ],
+          imageUrl: [
+            { required: true, message: '不能为空', trigger: 'blur' }
+          ],
+          imageIndex: [
+            { required: true, message: '不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.id = id || 0
+        this.visible = true
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+          if (this.dataForm.id) {
+            this.$http({
+              url: this.$http.adornUrl(`/generator/slide/info/${this.dataForm.id}`),
+              method: 'get',
+              params: this.$http.adornParams()
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.dataForm.imageName = data.slide.imageName
+                this.dataForm.imageUrl = data.slide.imageUrl
+                this.dataForm.imageIndex = data.slide.imageIndex
+              }
+            })
+          }
+        })
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/generator/slide/${!this.dataForm.id ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'imageName': this.dataForm.imageName,
+                'imageUrl': this.dataForm.imageUrl,
+                'imageIndex': this.dataForm.imageIndex
+              })
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
